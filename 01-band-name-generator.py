dddab839384aba756aa1c14f40ea52b18a98ae45
Eduardo Rosas <rosas.eduardovazquez@gmail.com>
2026-03-17 12:08:06 -0600

    Day 1: Band Name Generator

---
+++ 01-band-name-generator.py
@@ -0,0 +1,8 @@
+print(f"Hello!")
+
+city = input("What city were you born in?: ")
+pet = input("What is your pet's name?: ")
+
+combined_name = city + pet
+print(f"The combined name is: {combined_name}")
+
